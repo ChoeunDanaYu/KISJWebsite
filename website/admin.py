86f8db4b2b16a451cd980786286b2427d909d39f
ChoeunDanaYu <yudana0703@gmail.com>
2022-08-27 15:38:57 +0900

    set up admin page and made models for db

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Event, ImportantDates
+
+class EventAdmin(admin.ModelAdmin):
+    search_fields = ['event_name', 'content']
+
+admin.site.register(Event, EventAdmin)
+
+
+class ImportantDatesAdmin(admin.ModelAdmin):
+    search_fields = ['content']
+
+admin.site.register(ImportantDates, ImportantDatesAdmin)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+type_choices = (
+    ('Class Activity','Class Activity'),
+    ('Sports', 'Sports'),
+    ('Club','Club'),
+    ('Others','Others'),
+)
+
+class Event(models.Model):
+    event_type = models.CharField(max_length=200, choices=type_choices, default='Class Activity')
+    event_name = models.CharField(max_length=200)
+    teacher = models.CharField(max_length=200)
+    content = models.TextField()
+    event_image = models.TextField()
+    event_date = models.IntegerField()
+
+    def __str__(self):
+        return self.event_name
+
+class ImportantDates(models.Model):
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.create_date.strftime("%m/%d/%Y %H:%M:%S")
