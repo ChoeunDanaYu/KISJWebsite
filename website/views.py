d6defa91099760340636704f5ea6c259b1bf9e5c
ChoeunDanaYu <yudana0703@gmail.com>
2022-09-05 01:13:19 +0900

    created important dates function

--- website/views.py
+++ website/views.py
@@ -2,13 +2,30 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Event, ImportantDates
 from datetime import datetime, timedelta
+from .form import EventForm, ImportantDatesForm
 import boto3
 client = boto3.client('s3')
 
 # Create your views here.
 def index(request):
     context = {'daytype': generate_daytype(),'weektype': generate_weektype()}
+    impdate = ImportantDates.objects.latest('id').content
+    context['impdate'] = impdate
     return render(request, 'website/main.html', context)
+
+@login_required(login_url='common:login')
+def important_dates_edit(request):
+    if request.method == 'POST':
+        form = ImportantDatesForm(request.POST)
+        if form.is_valid():
+            impdate = form.save(commit=False)
+            impdate.create_date = datetime.now()
+            impdate.save()
+            return redirect('website:index')
+    else:
+        form = ImportantDatesForm(initial={'content': ImportantDates.objects.latest('id').content})
+    context = {'form': form}
+    return render(request, 'website/important_dates_form.html',context)
 
 
 """
