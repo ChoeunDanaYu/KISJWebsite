a40b45095cc2f36620a3561303bf9e4746b0a2c0
ChoeunDanaYu <yudana0703@gmail.com>
2022-09-05 01:11:18 +0900

    url  for important dates and html code completed

--- website/url.py
+++ website/url.py
@@ -6,5 +6,6 @@ app_name = 'website'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('event/create/', views.event_create, name='event_create')
+    path('event/create/', views.event_create, name='event_create'),
+    path('important-dates-edit/', views.important_dates_edit, name='important_dates_edit'),
 ]
